994b6f6c401c1c0f9c6214b5827b10018f793a13
Diego Rodríguez Barcos <diegomiragenil@gmail.com>
2021-10-14 11:12:58 +0200

    Tema2

---
+++ Tema2/ejercicio1.java
@@ -0,0 +1,19 @@
+public class ejercicio1 {
+  public static void main(String[] args) {
+    int x = 144; 
+    int y = 999;
+    int suma;
+    int resta;
+    int multiplicacion;
+    suma = (int) x + (int) y ;
+    resta = (int) x - (int) y ;
+    multiplicacion = (int) x * (int) y;
+    System.out.println("La suma de x + y es " + suma);
+    System.out.println("La resta de x - y es " + resta);
+    System.out.println("La multiplicacion de x * y es " + multiplicacion);
+    double division;
+    division = (double) x / (double) y ;
+    System.out.println("La división de x / y es " + division);
+
+  }
+}
---
+++ Tema2/ejercicio2.java
@@ -0,0 +1,10 @@
+public class ejercicio2 {
+  public static void main(String[] args) {
+
+    String nombre="Diego Rodríguez Barcos";
+    
+    System.out.println ("Nombre completo: " + nombre);
+  
+ 
+  }
+}
