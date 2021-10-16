b83d745391a9f6be0ac5b6fa61433f6173e2e7c8
Diego Rodríguez Barcos <diegomiragenil@gmail.com>
2021-10-16 18:41:49 +0200

    Ejercicio 4

---
+++ Tema2/ejercicio4.java
@@ -0,0 +1,16 @@
+public class ejercicio4 {
+  public static void main(String[] args) {
+
+    double euros=9;
+    
+    System.out.println("El número de euros que vamos a transformar en pesetas es " + euros);
+    
+    
+    
+    double valorpeseta=166.38;
+    
+    double pesetas= euros * valorpeseta;
+    
+    System.out.println (euros +" euros equivalen a " + pesetas + " pesetas");
+  }
+}
