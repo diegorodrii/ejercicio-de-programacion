d3c6508637dd69110ed0fea6789bf8676efec03d
Diego Rodríguez Barcos <diegomiragenil@gmail.com>
2021-10-16 18:21:06 +0200

    Ejercicio 3

---
+++ Tema2/ejercicio3.java
@@ -0,0 +1,15 @@
+public class ejercicio3 {
+  public static void main(String[] args) {
+
+    String nombre="Diego Rodríguez Barcos";
+    String direccion="Calle Esquilo 15-17";
+    String telefono="744471101";
+    
+    System.out.println ("Nombre completo: " + nombre);
+    System.out.println ("Nombre completo: " + direccion);
+    System.out.println ("Nombre completo: " + telefono);
+    
+  
+ 
+  }
+}
