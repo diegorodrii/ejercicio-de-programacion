fe45c733ca0e5acaeb8892d4f934584f9865267e
Diego Rodríguez Barcos <diegomiragenil@gmail.com>
2021-10-21 21:04:44 +0200

    Iniciación Tema Condicionales

--- Tema4/ejercicio01.java
+++ Tema4/ejercicio01.java
@@ -1,13 +1,41 @@
 /**
- * Realiza un conversor de Mb a Kb.
+ * Escribe un programa que pida por teclado un día de la semana y que diga qué
+ * asignatura toca a primera hora ese día.
  * 
  */
 
     
 public class ejercicio01 {
   public static void main(String[] args) {
-
+    System.out.println("Este programa printa por pantalla la asignatura que toca a primera hora dependiendo del día de la semana que se seleccione. ");
+    System.out.print("Por favor, indique un día de la semana con letra minúscula: ");
+    String dia = System.console().readLine();
     
+    if (dia.equals("lunes")) {
+      System.out.println("Programación");
+      
+    } 
+     if (dia.equals("martes")) {
+      System.out.println("Programación");
+      
+    }     
+    if (dia.equals("miercoles")) {
+      System.out.println("Sistemas informáticos");
+    }
+      
+    if (dia.equals("miércoles")) {
+      System.out.println("Sistemas informáticos");      
+      
+    }
+      
+    if (dia.equals("jueves")) {
+      System.out.println("Programación");
+      
+    } 
+     if (dia.equals("viernes")) {
+      System.out.println("Sistemas informáticos");
+         
+    }     
   }     
 }
 
