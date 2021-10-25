f003ce4f6c5aade1639856c5926c830ece849805
Diego Rodríguez Barcos <diegomiragenil@gmail.com>
2021-10-25 19:34:28 +0200

    Finalizacion

--- Tema4/ejercicio15.java
+++ Tema4/ejercicio15.java
@@ -12,17 +12,46 @@ public class ejercicio15{
   public static void main(String[] args) {
     System.out.println("Este programa pinta por pantalla una pirámide rellenada por el carácter que introduzca");
     System.out.print("Por favor, introduzca un caracter: ");
-    String pregunta1 = System.console().readLine();
+    String x = System.console().readLine();
 
     System.out.println("Por favor, introduzca la dirección hacia donde quiere que apunte el vértice de la pirámide: arriba , derecha, abajo, izquierda");
     String direccion = System.console().readLine();
     
-    if (direccion.equals("arriba"){
-      System.out.printf 
+    if (direccion.equals("arriba")){
+    System.out.println("\n    " + x + "    \n   " + x + x + x + "   ");
+    System.out.println("  " + x + x + x + x + x + "  ");
+    System.out.println(" "+ x + x + x + x + x + x + x + " ");
+    System.out.print("" + x + x + x + x + x + x);
+    System.out.print("" + x + x + x + "\n");
 
 
  
-        
+			}
+
+    if (direccion.equals("abajo")){
+		System.out.println("" + x + x + x +x +x + x + x + "");
+		System.out.println(" " + "" + x + x + x + x + x );
+		System.out.println(" "+ x + x + x + x + x + x + x + " ");
+		System.out.println("  " + x + x + x + x + x + "  ");
+		System.out.println("\n    " + x + "    \n   " + x + x + x + "   ");
+		
+			}
+    if (direccion.equals("derecha")){
+        System.out.println(x);
+        System.out.println(x + " " + x);
+        System.out.println(x + " " + x + " " + x);
+        System.out.println(x + " " + x);
+        System.out.println(x);
+		
+			}
+    if (direccion.equals("izquierda")){
+        System.out.println("    " + x);
+        System.out.println("  " + x + " " + x);
+        System.out.println(x + " " + x + " " + x);
+        System.out.println("  " + x + " " + x);
+        System.out.println("    " + x);
+		
+			}
   }
 }			
 
