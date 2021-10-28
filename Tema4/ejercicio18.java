35eddeff7c1be2dddb594a37938073f86d3198c3
Diego Rodríguez Barcos <diegomiragenil@gmail.com>
2021-10-28 08:48:27 +0200

    Seguimiento

---
+++ Tema4/ejercicio18.java
@@ -0,0 +1,19 @@
+/**
+ * Escribe un programa que diga cuál es la primera cifra de un número entero introducido por teclado. Se permiten números de hasta 5 cifras.
+ * 
+ * 
+ */
+ 
+public class ejercicio18{
+  public static void main(String[] args) {
+    System.out.println("Este programa te dice cuál es la primera cifra de un número entero de como mucho cinco cifras.");
+    System.out.print("Por favor, introduzca el número: ");
+    int numero = Integer.parseInt(System.console().readLine());
+
+
+
+ 
+        
+  }
+}			
+
