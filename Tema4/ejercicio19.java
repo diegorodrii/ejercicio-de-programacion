7782da9adeb6a6022fa2c967a925b5ec518cc6a2
Diego Rodríguez Barcos <diegomiragenil@gmail.com>
2021-10-28 09:09:47 +0200

    Ejercicios

--- Tema4/ejercicio19.java
+++ Tema4/ejercicio19.java
@@ -1,40 +1,66 @@
 /**
- * Escribe un programa que diga cuál es la primera cifra de un número entero introducido por teclado. Se permiten números de hasta 5 cifras.
+ * Realiza un programa que nos diga cuántos dígitos tiene un número entero que puede ser positivo o negativo. Se permiten números de hasta 5 dígitos.
  * 
  * 
  */
  
-public class ejercicio18{
+public class ejercicio19{
   public static void main(String[] args) {
-    System.out.println("Este programa te dice cuál es la primera cifra de un número entero de como mucho cinco cifras.");
+    System.out.println("Este programa te dice cuantos dígitos tiene un número introducido que puede ser positivo o negativo");
     System.out.print("Por favor, introduzca el número: ");
     int numero = Integer.parseInt(System.console().readLine());
     
+    if ((numero <= 0) && (numero >= -9)){
+			
+			System.out.println("El número de cifras del número introducido es 1");
+			
+			}
+			
+    if ((numero <= -10) && (numero >= -99)){
+			
+			System.out.println("El número de cifras del número introducido es 2");
+			
+			}
+    if ((numero <= -100) && (numero >= -999)){
+			
+			System.out.println("El número de cifras del número introducido es 3");
+			
+			}
+    if ((numero <= -1000) && (numero >= -9999)){
+			
+			System.out.println("El número de cifras del número introducido es 4");
+			
+			}
+    if ((numero <= -10000) && (numero >= -99999)){
+			
+			System.out.println("El número de cifras del número introducido es 5");
+			
+			}    
+    
     if ((numero >= 0) && (numero <= 9)){
 			
-			System.out.println("La primera cifra del número introducido es " + numero);
-			
+			System.out.println("El número de cifras del número introducido es 1");
 			
 			}
 			
     if ((numero >= 10) && (numero <= 99)){
 			
-			System.out.println("La primera cifra del número introducido es " + numero/10);
+			System.out.println("El número de cifras del número introducido es 2");
 			
 			}
     if ((numero >= 100) && (numero <= 999)){
 			
-			System.out.println("La primera cifra del número introducido es " + numero/100);
+			System.out.println("El número de cifras del número introducido es 3");
 			
 			}
     if ((numero >= 1000) && (numero <= 9999)){
 			
-			System.out.println("La primera cifra del número introducido es " + numero/1000);
+			System.out.println("El número de cifras del número introducido es 4");
 			
 			}
     if ((numero >= 10000) && (numero <= 99999)){
 			
-			System.out.println("La primera cifra del número introducido es " + numero/10000);
+			System.out.println("El número de cifras del número introducido es 5");
 			
 			}
 
