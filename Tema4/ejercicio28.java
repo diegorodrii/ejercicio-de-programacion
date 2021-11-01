e54a9de327d0a9d36738ae79ecfa9da22119fa10
Diego Rodríguez Barcos <diegomiragenil@gmail.com>
2021-11-01 14:18:40 +0100

    Mejora Ej28

--- Tema4/ejercicio28.java
+++ Tema4/ejercicio28.java
@@ -1,107 +1,82 @@
 /**
- * Una pastelería nos ha pedido realizar un programa que haga presupuestos de
- * tartas. El programa preguntará primero de qué sabor quiere el usuario la tarta:
- * manzana, fresa o chocolate. La tarta de manzana vale 18 euros y la de fresa
- * 16. En caso de seleccionar la tarta de chocolate, el programa debe preguntar
- * además si el chocolate es negro o blanco; la primera opción vale 14 euros y la
- * segunda 15. Por último se pregunta si se añade nata y si se personaliza con un
- * nombre; la nata suma 2.50 y la escritura del nombre 2.75.
+ * Implementa el juego piedra, papel y tijera. Primero, el usuario 1 introduce
+ * su jugada y luego el usuario 2. Si alguno de los usuarios introduce una opción
+ * incorrecta, el programa deberá mostrar un mensaje de error.
  */
  
-public class ejercicio27{
+public class ejercicio28{
   public static void main(String[] args) {
-    System.out.println("Este programa realiza presupuestos de tartas");
-    System.out.print("Por favor, díganos de que quiere el sabor de la tarta (1-manzana, 2-fresa o 3-chocolate): ");
-    int sabor = Integer.parseInt(System.console().readLine());
-    System.out.print("Por favor, díganos si quiere añadir nata a la tarta (1-Sí  2-No) ");
-    int nata = Integer.parseInt(System.console().readLine());
-    System.out.print("Por favor, díganos si quiere personalizar la tarta (1-Sí  2-No) ");
-    int personalizada = Integer.parseInt(System.console().readLine());
+    System.out.println("Este programa es un juego de piedra papel tijera de dos jugadores");
+    System.out.print("Por favor, díganos que escoge el primer jugador (1-piedra  2-papel  3-tijera): ");
+    int usuario1 = Integer.parseInt(System.console().readLine());
+    System.out.print("Por favor, díganos que escoge el segundo jugador (1-piedra  2-papel  3-tijera): ");
+    int usuario2 = Integer.parseInt(System.console().readLine());
 
-    double precioManzana= 18;
-    double precioFresa = 16;
-    double precioChocolateNegro = 14;
-    double precioChocolateBlanco = 15;
-    double precioNata = 2.5;
-    double precioNombre = 2.75;
-    double precioFinal = 0;
 
-    String chocolateEscrito = "";
-    /**Sabor */
-    if (sabor == 1){
-      precioFinal = precioManzana;
-    } else if (sabor == 2){
-      precioFinal = precioFresa;
-    } else if (sabor == 3){
-      System.out.print("¿El chocolate desea que sea negro(1) o blanco(2)?");
-      int tipoChocolate = Integer.parseInt(System.console().readLine());
-      if (tipoChocolate == 1){
-        precioFinal = precioChocolateNegro;
-        chocolateEscrito = "negro";
-        
-      } else if (tipoChocolate == 2)
-        precioFinal = precioChocolateBlanco;
-        chocolateEscrito = "blanco";
-    }
-    /**Nata */
-    if (nata == 1){
-      precioFinal = precioFinal + precioNata;
-    }
-
-    /**Nombre */
-    if (personalizada == 1){
-      precioFinal = precioFinal + precioNombre;
-    }
-  
-    /** Conversión int a String */
-    String tipoSabor = "";
-    switch (sabor){
+    /**Elecciones */
+    String eleccion1 = "";
+    switch(usuario1){
       case 1:
-      tipoSabor = "manzana";
+      eleccion1 = "El jugador 1 elige piedra";
       break;
- 
+
       case 2:
-      tipoSabor = "fresa";
+      eleccion1 = "El jugador 1 elige papel";
       break;
 
       case 3:
-      tipoSabor = "chocolate";
+      eleccion1 = "El jugador 1 elige tijera";
       break;
-    }
-    String nataAñadida = "";
-    switch (nata){
+  
+      default:
+      eleccion1 = "Esta opción no es correcta";
+    }   
+    String eleccion2 = "";
+    switch(usuario2){
       case 1:
-      nataAñadida = "sí";
+      eleccion2 = "El jugador 2 elige piedra";
       break;
- 
-      case 2:
-      nataAñadida = "no";
-      break;
-    }
-    String nombreAñadido = "";
-    switch (personalizada){
-      case 1:
-      nombreAñadido = "sí";
-      break;
- 
-      case 2:
-      nombreAñadido= "no";
-      break;
-    }
 
-    /** Resolución */
-    if (chocolateEscrito.equals("negro")){
-      System.out.println("Sabor: chocolate negro");
-    } else if (chocolateEscrito.equals("blanco")){
-      System.out.println("Sabor: chocolate blanco");
-    } else {
-      System.out.println("Sabor: " + tipoSabor);
+      case 2:
+      eleccion2 = "El jugador 2 elige papel";
+      break;
+
+      case 3:
+      eleccion2 = "El jugador 2 elige tijera";
+      break;
+      
+      default: 
+      eleccion2 = "Esta opción no es correcta";
     }
+ 
+    /**Combates */
+    String resultado = "";
+    if ((usuario1 == 1) && (usuario2 == 2)){
+      resultado = "Gana el Jugador 2";
+    } else if ((usuario1 == 1) && (usuario2 == 3)){
+      resultado = "Gana el Jugador 1";
+    } if ((usuario1 == 2) && (usuario2 == 1)){
+      resultado = "Gana el Jugador 1";
+    } else if ((usuario1 == 2) && (usuario2 == 3)){
+      resultado = "Gana el Jugador 2";
+    }else if ((usuario1 == 3) && (usuario2 == 1)){
+      resultado = "Gana el Jugador 2";
+    } else if ((usuario1 == 3) && (usuario2 == 2)){
+      resultado = "Gana el Jugador 1";
+    } 
+
    
-    System.out.println("Nata: " + nataAñadida);
-    System.out.println("Personalización: " + nombreAñadido);
-    System.out.println("Precio final: " + precioFinal);
-
+    System.out.println(eleccion1);
+    System.out.println(eleccion2);
+    if ((usuario1 == 1) && (usuario2 == 1)){
+      System.out.println("El resultado es empate");
+    } else if ((usuario1 == 2) && (usuario2 == 2)){
+      System.out.println("El resultado es empate");
+    } else if ((usuario1 == 3) && (usuario2 == 3)){
+      System.out.println("El resultado es empate");
+    } else {
+      System.out.println(resultado);
+    }
   } 
 }			
 
