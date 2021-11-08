fae1767599e1db7902b4690ae70060d83c589743
Diego Rodríguez Barcos <diegomiragenil@gmail.com>
2021-11-08 09:28:09 +0100

    Iniciación tema 5

---
+++ Tema5/Ejercicio01.java
@@ -0,0 +1,12 @@
+/**
+ * Muestra los números múltiplos de 5 de 0 a 100 utilizando un bucle for .
+ *
+ * @author Diego Rodríguez Barcos
+ */
+public class Ejercicio01 {
+  public static void main(String[] args) {
+    for (int i = 5; i <= 100; i += 5) {
+      System.out.println(i);
+    }
+  }
+}
---
+++ Tema5/Ejercicio02.java
@@ -0,0 +1,12 @@
+/**
+ * Muestra los números del 320 al 160, contando de 20 en 20 hacia atrás utilizando un bucle for .
+ *
+ * @author Diego Rodríguez Barcos
+ */
+public class Ejercicio02 {
+  public static void main(String[] args) {
+    for (int i = 320; i >= 160; i -= 20) {
+      System.out.println(i);
+    }
+  }
+}
