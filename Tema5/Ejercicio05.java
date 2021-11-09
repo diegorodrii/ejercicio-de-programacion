5e0da2af51a663835d5e50b546c671b3890a1e09
Diego Rodríguez Barcos <diegomiragenil@gmail.com>
2021-11-09 10:09:24 +0100

    Seguimiento ejercicios

--- Tema5/Ejercicio05.java
+++ Tema5/Ejercicio05.java
@@ -1,14 +1,14 @@
 /**
- * Muestra los números múltiplos de 5 de 0 a 100 utilizando un bucle do-while .
+ * Muestra los números del 320 al 160, contando de 20 en 20 hacia atrás utilizando un bucle while .
  *
  * @author Diego Rodríguez Barcos
  */
 public class Ejercicio05 {
   public static void main(String[] args) {
-    int i = 0;
-    do {
+    int i = 320;
+    while (i >= 160){
       System.out.println(i);
-      i += 5;
-    } while (i <= 100);
+      i -= 20;
+    } 
   }
 }
