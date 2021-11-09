08a6833c32bc49ad324640ec8b24bac0125f9b24
Diego Rodríguez Barcos <diegomiragenil@gmail.com>
2021-11-09 11:17:15 +0100

    Seguimiento

---
+++ Tema5/Ejercicio09.java
@@ -0,0 +1,32 @@
+import javax.swing.text.Document;
+
+/**
+ * Realiza un programa que nos diga cuántos dígitos tiene un número introducido
+ * por teclado. Este ejercicio es equivalente a otro realizado anteriormente, con
+ * la salvedad de que el anterior estaba limitado a números de 5 dígitos como
+ * máximo. En esta ocasión, hay que realizar el ejercicio utilizando bucles; de
+ * esta manera, la única limitación en el número de dígitos la establece el tipo de
+ * dato que se utilice ( int o long ).
+ *
+ * @author Diego Rodríguez Barcos
+ */
+public class Ejercicio09 {
+  public static void main(String[] args) {
+   
+    int numero = 0;
+    System.out.print("Este programa te dice cuántos dígitos tiene un número introducido por teclado: ");
+    numero = Integer.parseInt(System.console().readLine());
+
+    
+    int cantidadDivisiones = 0;
+    do {
+      numero = numero / 10;
+      cantidadDivisiones++;
+    } while (numero > 0);
+
+  System.out.println("Tu número tiene " + cantidadDivisiones + " dígitos");
+
+
+    
+  }
+}
