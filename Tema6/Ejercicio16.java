4a08ef9d3a5b149eb12fc606ecada2dc28986a17
Diego Rodríguez Barcos <diegomiragenil@gmail.com>
2021-12-08 11:44:43 +0100

    Ej16

--- Tema6/Ejercicio16.java
+++ Tema6/Ejercicio16.java
@@ -16,47 +16,76 @@ public class Ejercicio16 {
   public static void main(String[] args) {
     System.out.println("Este programa es una máquina tragaperras");
 
+   
     int icono1 = 0;
     int icono2 = 0;
     int icono3 = 0;
 
     for (int i = 0; i < 3; i++) {
-      int figura = (int) (Math.random() * 5);
+      int figura = (int) ((Math.random() * 5) + 1);
       String dibujo = "";
       switch (figura) {
         case 1:
           dibujo = "❤️";
-          icono1 = figura;
-          icono2 = figura;
-          icono3 = figura;
+         
           break;
 
         case 2:
           dibujo = "♦️";
-          icono2 = figura;
+        
           break;
 
         case 3:
           dibujo = "🦄";
-          icono2 = figura;
+          
           break;
 
         case 4:
           dibujo = "🔔";
-          icono2 = figura;
+         
           break;
 
         case 5:
-          dibujo = "🍋";
-          icono2 = figura;
+          dibujo = "⭐";
+          
           break;
           
       }
-      figura 
+      
+      if (i == 0){
+        icono1 = figura;
+      } else if (i == 1){
+        icono2 = figura;
+      } else if (i == 2){
+        icono3 = figura;
+      }
 
+     
       System.out.print(dibujo);
       System.out.print(" ");
     }
+
+    System.out.println();
+    
+    if ((icono1 != icono2) && (icono2 != icono3) && (icono3 != icono1)){
+      System.out.println("Lo siento, ha perdido");
+    }
+    /** Si son dos iguales */
+    if ((icono2 != icono3) && (icono1 == icono3) && (icono2 != icono1)){
+      System.out.println("Ha recuperado su moneda!");
+    }
+    if ((icono1 != icono3) && (icono2 == icono3) && (icono2 != icono1)){
+      System.out.println("Ha recuperado su moneda!");
+    }
+    if ((icono3 != icono2) && (icono1 == icono2) && (icono3 != icono1)){
+      System.out.println("Ha recuperado su moneda!");
+    }
+    
+    /** Si son 3 iguales */
+    if ((icono1 == icono2) && (icono2 == icono3)){
+      System.out.println("Enhorabuena, ha ganado 10 monedas!");
+    }
+
     System.out.println();
 
 
