ab91662dbb777ccf78494a061c427484af869bd8
Diego Rodríguez Barcos <diegomiragenil@gmail.com>
2022-01-03 15:15:03 +0100

    Ejercicio2 actualización

--- Tema7/Arrays bidimensionales/Ejercicio02.java
+++ Tema7/Arrays bidimensionales/Ejercicio02.java
@@ -8,51 +8,49 @@
  */
 public class Ejercicio02 {
   public static void main(String[] args) {
-    int[][] num = new int[4][5]; // array de 4 filas por 5 columnas
-    
+
+    System.out.println("Este programa pinta una tabla y la suma de columnas y filas");
+
+    int[][] num = new int[4][5];
+
     int fila;
     int columna;
-    
-    // Lee los datos de teclado
-    System.out.println("Por favor, introduzca los números (enteros) en el array");
-    for(fila = 0; fila < 4; fila++) {
-      for(columna = 0; columna < 5; columna++) {
-        System.out.print("Fila " + fila + ", columna " + columna + ": ");
+
+    for (fila = 0; fila < 4; fila++) {
+      for (columna = 0; columna < 5; columna++) {
+        System.out.print("Fila " + fila + "  Columna " + columna + " :  ");
         num[fila][columna] = Integer.parseInt(System.console().readLine());
       }
     }
 
-    // Muestra los datos y las sumas parciales de las filas
+    // Enseña filas y columnas y las filas las suma*/
     int sumaFila;
-    for(fila = 0; fila < 4; fila++) {
+    for (fila = 0; fila < 4; fila++) {
       sumaFila = 0;
-      for(columna = 0; columna < 5; columna++) {
-        System.out.printf("%7d   ", num[fila][columna]);
+      for (columna = 0; columna < 5; columna++) {
+        System.out.printf("%7d", num[fila][columna]);
         sumaFila += num[fila][columna];
       }
-      System.out.printf("|%7d\n", sumaFila);
-    }
-    
-    // Muestra las sumas parciales de las columnas
-    for(columna = 0; columna < 5; columna++) {
-      System.out.print("----------");
-    }
-    System.out.println("-----------");
 
+      System.out.print("   |");
+      System.out.printf("%7d \n", sumaFila);
+    }
+
+    // Enseña suma columnas y suma total*/
     int sumaColumna;
     int sumaTotal = 0;
-    for(columna = 0; columna < 5; columna++) {
+    System.out.println("-------------------------------------------------");
+
+    for (columna = 0; columna < 5; columna++) {
       sumaColumna = 0;
-      for(fila = 0; fila < 4; fila++) {
+      for (fila = 0; fila < 4; fila++) {
         sumaColumna += num[fila][columna];
+        sumaTotal += num[fila][columna];
       }
 
-      sumaTotal += sumaColumna;
-      System.out.printf("%7d   ", sumaColumna);
+      System.out.printf("%7d", sumaColumna);
     }
-    System.out.printf("|%7d   ", sumaTotal);
+    System.out.printf("   |%7d ", sumaTotal);
+
   }
 }
-
-    
- 
