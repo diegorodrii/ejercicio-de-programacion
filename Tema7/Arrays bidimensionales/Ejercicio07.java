38c65de574d759d850488b0221d58468c68549ba
Diego Rodríguez Barcos <diegomiragenil@gmail.com>
2022-01-13 09:12:31 +0100

    EJ 7

--- Tema7/Arrays bidimensionales/Ejercicio07.java
+++ Tema7/Arrays bidimensionales/Ejercicio07.java
@@ -77,6 +77,9 @@ public class Ejercicio07 {
       switch(cuadrante[x][y]) {
         case VACIO:
           cuadrante[x][y] = INTENTO;
+          if ((Math.abs(x - minaX) < 2) && (Math.abs(y - minaY) < 2)) {
+            System.out.println("Cuidado, hay una mina cerca.");
+          }
           break;
         case MINA:
           System.out.println("Lo siento, has perdido.");
